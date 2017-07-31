3654ea6d89ad67ddc7c24b9a1fe76b5bea06ba0b
alexander-liao <alex.yj.liao@gmail.com>
2017-07-31 19:14:36 -0400

    removed testing code

--- interpreter.py
+++ interpreter.py
@@ -5,18 +5,10 @@ from modeselector import select
 from stack import Stack
 
 def interpret(functions, arguments):
-	for function in functions:
-		s = Stack(None)
-		try:
-			function(s, arguments)
-			print(function, s)
-		except:
-			print(function, 'requires things on the stack or is broken')
 	stack = Stack(arguments)
 	print(stack)
 	for function in functions:
 		function(stack, arguments)
-		print(stack)
 	return stack
 
 def evaluate(code, arguments):
